3efe2ea0384560851c74cbd9305277447b42b2e2
Matt Heerspink <mheerspink75@gmail.com>
2019-10-29 15:30:03 -0500

    added comments to app.js

--- src/app.js
+++ src/app.js
@@ -1,23 +1,28 @@
+// npm install webpack --save-dev
+// npm install exports-loader --save-dev
+// npm install autoprefixer css-loader node-sass postcss-loader sass-loader style-loader --save-dev
 import 'bootstrap';
 import './scss/app.scss';
 
+// npm install imports-loader
 var $ = require("jquery");
 
+// GIPHY API SEARCH - jQuery code
 const gifForm = $("#gif-form");
 
 gifForm.submit(e => {
-  e.preventDefault();
-  const searchTerm = $(".search").val();
-  const url = `https://api.giphy.com/v1/gifs/search?&q=${searchTerm}&limit=80&api_key=3mIxmBZUIIPyb8R69gtxaW8Hsh74dFKV`;
-  $.get(url).done(resp => {
-      showGiphs(resp.data.slice(0, 40));
+    e.preventDefault();
+    const searchTerm = $(".search").val();
+    const url = `https://api.giphy.com/v1/gifs/search?&q=${searchTerm}&limit=80&api_key=3mIxmBZUIIPyb8R69gtxaW8Hsh74dFKV`;
+    $.get(url).done(resp => {
+        showGiphs(resp.data.slice(0, 40));
     })
-    .fail(console.log);
+        .fail(console.log);
 });
 
-function showGiphs(dataArray) { 
-  const results = document.querySelector(".results");
-  let output = "";
-  output = dataArray.map(imgData => `<a href="${imgData.images.original.url}" alt="${imgData.title}" target="_blank"><img src="${imgData.images.original.url}"></a>`).join("");
-  $(".results").html(output);
+function showGiphs(dataArray) {
+    const results = document.querySelector(".results");
+    let output = "";
+    output = dataArray.map(imgData => `<a href="${imgData.images.original.url}" alt="${imgData.title}" target="_blank"><img src="${imgData.images.original.url}"></a>`).join("");
+    $(".results").html(output);
 }
