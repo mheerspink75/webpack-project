cf807c02b2c085e9824dfbadd7ae6d0bcace42f7
Matt Heerspink <mheerspink75@gmail.com>
2019-10-29 15:51:30 -0500

    changed formatting and added comments

--- src/app.js
+++ src/app.js
@@ -1,28 +1,9 @@
-// npm install webpack --save-dev
 // npm install exports-loader --save-dev
 import 'bootstrap';
+
 // npm install autoprefixer css-loader node-sass postcss-loader sass-loader style-loader --save-dev
 import './scss/app.scss';
 
 // npm install imports-loader
-var $ = require("jquery");
+import "./giphy_search.js";
 
-// GIPHY API SEARCH - jQuery code
-const gifForm = $("#gif-form");
-
-gifForm.submit(e => {
-    e.preventDefault();
-    const searchTerm = $(".search").val();
-    const url = `https://api.giphy.com/v1/gifs/search?&q=${searchTerm}&limit=80&api_key=3mIxmBZUIIPyb8R69gtxaW8Hsh74dFKV`;
-    $.get(url).done(resp => {
-        showGiphs(resp.data.slice(0, 40));
-    })
-        .fail(console.log);
-});
-
-function showGiphs(dataArray) {
-    const results = document.querySelector(".results");
-    let output = "";
-    output = dataArray.map(imgData => `<a href="${imgData.images.original.url}" alt="${imgData.title}" target="_blank"><img src="${imgData.images.original.url}"></a>`).join("");
-    $(".results").html(output);
-}
