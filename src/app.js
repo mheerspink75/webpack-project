0041114b359b46b6bfc539046884205f6978367b
Matt Heerspink <mheerspink75@gmail.com>
2020-05-03 19:24:44 -0500

    renamed and relocated files

--- src/app.js
+++
@@ -1,8 +0,0 @@
-// npm install exports-loader --save-dev
-import 'bootstrap';
-
-// npm install autoprefixer css-loader node-sass postcss-loader sass-loader style-loader --save-dev
-import './scss/app.scss';
-
-// npm install imports-loader
-import "./giphy_search.js";
