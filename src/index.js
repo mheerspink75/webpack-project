a23534283dcabeb4aad8f40ff5e12f8a791f26dd
Matt Heerspink <mheerspink75@gmail.com>
2020-05-03 19:22:11 -0500

    updated index.js for new folder structure

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// npm install exports-loader --save-dev
+import 'bootstrap';
+
+// npm install autoprefixer css-loader node-sass postcss-loader sass-loader style-loader --save-dev
+import './scss/style.scss';
+
+// npm install imports-loader
+import "./js/giphy_search.js";
